2482ac9e981077ccc917d6c6ee8ff578278cc9e9
arafa0001 <abdulrafayimran4@gmail.com>
2026-01-12 06:25:12 +0500

    created first route

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("backend is runnnnnnnnnning....")
 })
+app.get("/api/health", (req, res) => {
+    res.json(
+        {
+    status: "ok",
+    message: "API is healthy"
+    })
+})
 
 app.listen(5000, () => {
     console.log("Server running on http://localhost:5000");
